dea02efba9073ae7487af572c0e78ed4b511cfcf
Thilo Bangert <thilo@bangert.dk>
2025-01-02 21:08:40 +0100

    change to snake case

--- pyandelenergi/__init__.py
+++ pyandelenergi/__init__.py
@@ -3,26 +3,26 @@ from urllib.request import urlopen
 import csv
 from datetime import date, datetime, timedelta
 
-def getDefaultData():
-    startDate = getEarliestStartDate()
-    endDate = getMaxFutureDate()
-    defaultRegion = "east"
-    url = buildUrl(defaultRegion, startDate, endDate)
-    return fetchData(url)
+def get_default_data():
+    start_date = get_earliest_start_date()
+    end_date = get_max_future_date()
+    default_region = "east"
+    url = build_url(default_region, start_date, end_date)
+    return fetch_data(url)
 
-def getEarliestStartDate():
+def get_earliest_start_date():
     # today
-    startDate = date.today()
-    return startDate.strftime("%Y-%m-%d")
+    start_date = date.today()
+    return start_date.strftime("%Y-%m-%d")
 
-def getMaxFutureDate():
-    futureDate = date.today() + timedelta(days=2)
-    return futureDate.strftime("%Y-%m-%d")
+def get_max_future_date():
+    future_date = date.today() + timedelta(days=2)
+    return future_date.strftime("%Y-%m-%d")
 
-def buildUrl(region, startDate, endDate):
-    return f"https://andelenergi.dk/?obexport_format=csv&obexport_start={startDate}&obexport_end={endDate}&obexport_region={region}&obexport_tax=0&obexport_product_id=1%231%23TIMEENERGI"
+def build_url(region, start_date, end_date):
+    return f"https://andelenergi.dk/?obexport_format=csv&obexport_start={start_date}&obexport_end={end_date}&obexport_region={region}&obexport_tax=0&obexport_product_id=1%231%23TIMEENERGI"
 
-def fetchData(url):
+def fetch_data(url):
     # download csv
     with urlopen(url) as response:
         result = dict()
